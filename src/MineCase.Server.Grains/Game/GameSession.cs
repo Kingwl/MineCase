0912b9904e7fceecdd4608e99083dbfa99c7fed0
SunnyCase <sunnycase@live.cn>
2017-08-22 15:54:22 +0800

    Add map generation.

--- src/MineCase.Server.Grains/Game/GameSession.cs
+++ src/MineCase.Server.Grains/Game/GameSession.cs
@@ -13,6 +13,7 @@ namespace MineCase.Server.Game
     internal class GameSession : Grain, IGameSession
     {
         private IWorld _world;
+        private IChunkSender _chunkSender;
         private readonly Dictionary<IUser, UserContext> _users = new Dictionary<IUser, UserContext>();
 
         private IDisposable _gameTick;
@@ -21,6 +22,7 @@ namespace MineCase.Server.Game
         public override async Task OnActivateAsync()
         {
             _world = await GrainFactory.GetGrain<IWorldAccessor>(0).GetWorld(this.GetPrimaryKeyString());
+            _chunkSender = GrainFactory.GetGrain<IChunkSender>(Guid.NewGuid());
             _lastGameTickTime = DateTime.UtcNow;
             _gameTick = RegisterTimer(OnGameTick, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(50));
         }
